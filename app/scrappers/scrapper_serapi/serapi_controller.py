f09dec8928cdb5052a9eb9951c8f750c06fd5919
lucasfbb <lucasfirmino2612@gmail.com>
2025-11-15 19:22:38 -0300

    SerAPI-fix: ajustando rota

--- app/scrappers/scrapper_serapi/serapi_controller.py
+++ app/scrappers/scrapper_serapi/serapi_controller.py
@@ -2,14 +2,14 @@ from fastapi import APIRouter, Depends
 from app.scrappers.scrapper_serapi import scrapper_serapi_dto
 from app.scrappers.scrapper_serapi import serapi_service
 
-router = APIRouter(tags=["SerAPI Google Maps"])
+serapi_router = APIRouter(tags=["SerAPI Google Maps"])
 
 
-@router.post("/buscar")
+@serapi_router.post("/buscar")
 def buscar(data: scrapper_serapi_dto, service: serapi_service = Depends()):
     return service.buscar(data.termo)
 
 
-@router.post("/buscar/excel")
+@serapi_router.post("/buscar/excel")
 def buscar_excel(data: scrapper_serapi_dto, service: serapi_service = Depends()):
     return service.buscar_excel(data.termo)
